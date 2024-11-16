6ab020a927628bcf9c004ea856cdf494cf99128a
Solstice <leon.t.isip@gmail.com>
2024-11-17 07:41:53 +0800

    complete functionality, will adjust few more details in a bit

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ const userInput = document.getElementById('text-input');
 const checkPalindromeBtn = document.getElementById('check-btn');
 const resultDiv = document.getElementById('results');
 
-checkPalindromeBtn.addEventListener("click", myFunction);
+checkPalindromeBtn.addEventListener("click", checkIfEmpty);
 
-function myFunction() {
+function checkIfEmpty() {
+    if(userInput.value === "") {
+        alert('Please enter a valid input');
+    }
+
+    // let userInput = userInput.value.trim().toLowerCase();
+    // let reversedUserInput = userInput.split('').reverse().join('');
+
+    const inputStr = userInput.value.trim().toLowerCase();
+    const reversedStr = inputStr.split('').reverse().join('');
+
+    if(inputStr === reversedStr) {
+        resultDiv.replaceChildren(inputStr + " is a palindrome");
+    }else{
+        resultDiv.replaceChildren(inputStr + " is not a palindrome");
+    }
 }
+
+
+
