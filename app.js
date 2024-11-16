24fc28d5045e3f74e676c1ae0420631de092ea3c
Solstice <leon.t.isip@gmail.com>
2024-11-17 06:53:48 +0800

    integrating javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+const userInput = document.getElementById('user-input');
+const checkPalindromeBtn = document.getElementById('check-btn');
+const resultDiv = document.getElementById('results');
+
+
+
+checkPalindromeBtn.addEventListener('click', () => {resultDiv.replaceChildren("rawr")})
